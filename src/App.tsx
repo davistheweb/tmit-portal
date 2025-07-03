5632cefcbee54162e0793b80957bb0766b64af29
davistheweb <josiahdave001@gmail.com>
2025-07-03 13:01:13 +0100

    :

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-function App() {
-}
+import * as React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="w-full h-screen overflow-x-hidden bg-white text-black"></div>
+  );
+};
 
 export default App;
---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+import schoolLogo from "./images/schoolLogo.png";
+import authBackground from "./images/School_Entrance_Image.jpg";
+
+export { schoolLogo, authBackground };

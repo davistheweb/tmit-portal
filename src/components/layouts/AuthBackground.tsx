01de2131444f5c90314c8e3169549bb13c2ede21
davistheweb <josiahdave001@gmail.com>
2025-07-05 21:47:39 +0100

    :

--- src/components/layouts/AuthBackground.tsx
+++ src/components/layouts/AuthBackground.tsx
@@ -7,9 +7,9 @@ export const AuthBackground: React.FC<{ children?: React.ReactNode }> = ({
   return (
     <div className="bg-green-500 relative flex items-center justify-center md:justify-start w-full h-screen overflow-y-hidden overflow-x-hidden">
       <div
-          className="absolute md:hidden w-full h-screen bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: `url(${authBackground})` }}
-        />
+        className="absolute md:hidden w-full h-screen bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${authBackground})` }}
+      />
       <div className="absolute right-0 top-0 hidden md:flex">
         <div>
           <img

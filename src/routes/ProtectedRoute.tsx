c1d873504adb20c1ea4cf7fc6a40957125d1feab
davistheweb <josiahdave001@gmail.com>
2025-07-13 11:52:07 +0100

    updated protected route logic

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate } from "react-router";
+import { Navigate, useLocation } from "react-router";
 
 export const ProtectedRoute = ({
   children,
@@ -7,6 +7,21 @@ export const ProtectedRoute = ({
   children: React.JSX.Element;
 }) => {
   const token = localStorage.getItem("token");
+  const profileCompleted = localStorage.getItem("profile_completed");
 
-  return token ? children : <Navigate to="/auth/login" replace />;
+  const location = useLocation();
+
+  if (!token) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
+  const isAccessingOnboarding = location.pathname.includes(
+    "/dashboard/onboarding"
+  );
+
+  if (profileCompleted !== "true" && !isAccessingOnboarding) {
+    return <Navigate to="/dashboard/onboarding" replace />;
+  }
+
+  return children;
 };
